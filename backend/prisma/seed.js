a9c7a6060c407e0a4afb0e10f7463db3eebfd246
Wannzh <alwanfdhlrhmn@gmail.com>
2026-05-25 19:54:00 +0700

    ignore env files, seed.js

--- backend/prisma/seed.js
+++ backend/prisma/seed.js
@@ -6,7 +6,7 @@ const { PrismaPg } = require("@prisma/adapter-pg");
 const connectionString = process.env.DIRECT_URL || process.env.DATABASE_URL;
 
 if (!connectionString) {
-throw new Error("DIRECT_URL / DATABASE_URL belum ada di .env");
+  throw new Error("DIRECT_URL / DATABASE_URL belum ada di .env");
 }
 
 const pool = new Pool({ connectionString });
@@ -14,24 +14,24 @@ const adapter = new PrismaPg(pool);
 const prisma = new PrismaClient({ adapter });
 
 async function main() {
-console.log("Seeding...");
-await prisma.category.createMany({
-data: [
-{ name: "Makanan", icon: "🍔", color: "#f97316", isDefault: true },
-{ name: "Transport", icon: "🛵", color: "#3b82f6", isDefault: true },
-{ name: "Belanja", icon: "🛒", color: "#10b981", isDefault: true },
-],
-skipDuplicates: true,
-});
-console.log("✅ Seed selesai");
+  console.log("Seeding...");
+  await prisma.category.createMany({
+    data: [
+      { name: "Makanan", icon: "🍔", color: "#f97316", isDefault: true },
+      { name: "Transport", icon: "🛵", color: "#3b82f6", isDefault: true },
+      { name: "Belanja", icon: "🛒", color: "#10b981", isDefault: true },
+    ],
+    skipDuplicates: true,
+  });
+  console.log("✅ Seed selesai");
 }
 
 main()
-.catch((e) => {
-console.error("❌ Seed error:", e);
-process.exit(1);
-})
-.finally(async () => {
-await prisma.$disconnect();
-await pool.end();
-});
+  .catch((e) => {
+    console.error("❌ Seed error:", e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+    await pool.end();
+  });
